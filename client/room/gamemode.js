488fa546f773a493f46d7fb063359d370e79ecdd
ƬN丅 ｌlᴠɆ (ᴼᶠᶠⁱᶜⁱᵃˡ) <145314654+hoycmd@users.noreply.github.com>
2025-10-13 20:54:06 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -65,8 +65,13 @@ Damage.OnDeath.Add(function(p) {
   p.Ui.Hint.Value = "Ожидайте, конца матча!";
    p.Spawns.Despawn();
    p.Spawns.RespawnEnable.Value = false;
-   if (blueTeam.Properties.Deaths.Value == 1) {
-    WinRedTeam();
-   }
-   if (redTeam.Properties.Deaths.Value == 1) {
-    WinBlueTeam();
+       if (blueTeam.Properties.Deaths.Value == 1) {
+       WinRedTeam();
+      }
+       if (redTeam.Properties.Deaths.Value == 1) {
+        WinBlueTeam();
+     }
+});
+
+// счетчик убийств
+Damage.OnKill.Add(function(k,p) {
