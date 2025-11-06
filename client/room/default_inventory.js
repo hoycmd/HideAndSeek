338df3e7ca6ffb4cfc62e83ac8f35e7d7ff182a3
ƬN丅 ｌlᴠɆ (ᴼᶠᶠⁱᶜⁱᵃˡ) <145314654+hoycmd@users.noreply.github.com>
2025-11-06 16:25:44 +0700

    Update default_inventory.js

--- client/room/default_inventory.js
+++ client/room/default_inventory.js
@@ -1,6 +1,9 @@
 import { GameMode, Inventory } from 'pixel_combats/room';
 
+export function BV() {
 switch (GameMode.Parameters.GetBool('Blue')) {
    case 'PNusto': blueTeam.Inventory.Melee.Value = false; break;
    case 'Melee': blueTeam.Inventory.Melee.Value = true; break;
+   }
+   return PNusto;
 }
