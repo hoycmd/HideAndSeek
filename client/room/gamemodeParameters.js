204b7d5fdb15dcdb7adbb5b9f4be2970348ebf57
ƬN丅 ｌlᴠɆ (ᴼᶠᶠⁱᶜⁱᵃˡ) <145314654+hoycmd@users.noreply.github.com>
2025-11-06 17:36:01 +0700

    Update and rename default_timer.js to gamemodeParameters.js

--- client/room/gamemodeParameters.js
+++ client/room/gamemodeParameters.js
@@ -3,6 +3,7 @@ import { GameMode } from 'pixel_combats/room';
 
 // * Константы имён, объектов. * //
 const length = GameMode.Parameters.GetString('GameLength');
+const Blue = GameMode.Parameters.GetString('BlueEvent');
 
 // * Длинна, матча. * //
 export function GameModeMatchTime() {
@@ -13,3 +14,11 @@ export function GameModeMatchTime() {
     }   
     return 421;
 }
+
+let SelectedDefaultInventoryBlue;
+export function InventoryBlueEvent() {
+ switch (Blue) {
+     case 'PNusto': SelectedDefaultInventoryBlue = false; break;
+     case 'Melee': SelectedDefaultInventoryBlue = true; break;
+}
+export const DefultInventory = SelectedDefaultInventoryBlue;
