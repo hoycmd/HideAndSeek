c7ac7bf81f67fde8dcbde63e3616a2fb1bf93a32
ƬN丅 ｌlᴠɆ (ᴼᶠᶠⁱᶜⁱᵃˡ) <145314654+hoycmd@users.noreply.github.com>
2025-11-06 17:39:13 +0700

    Delete client/room/default_inventory.js

--- client/room/default_inventory.js
+++
@@ -1,9 +0,0 @@
-import { GameMode, Inventory } from 'pixel_combats/room';
-
-export function BV() {
-switch (GameMode.Parameters.GetBool('Blue')) {
-   case 'PNusto': blueTeam.Inventory.Melee.Value = false; break;
-   case 'Melee': blueTeam.Inventory.Melee.Value = true; break;
-   }
-   return PNusto;
-}
