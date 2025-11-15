82e42a3d7b3c61ac177fa589e354a05fad31948b
ƬN丅 ｌlᴠɆ (ᴼᶠᶠⁱᶜⁱᵃˡ) <145314654+hoycmd@users.noreply.github.com>
2025-11-15 19:27:43 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -122,7 +122,7 @@ if (stateProp.Value != HideAndSeekStateValue && stateProp.Value != WaitingModeSt
 	          }
     } 
 });
-daedTimer.RestartLoop(1);
+daedTimer.RestartLoop(3);
 	
 // * Основной таймер, переключения режимов игры. * //
 mainTimer.OnTimer.Add(function() {
