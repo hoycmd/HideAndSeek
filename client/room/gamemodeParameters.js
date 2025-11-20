e2e6d71475bfda9b7fa52a8d3a52db3de749e81a
ƬN丅 ｌlᴠɆ (ᴼᶠᶠⁱᶜⁱᵃˡ) <145314654+hoycmd@users.noreply.github.com>
2025-11-20 20:05:35 +0700

    Delete client/room/gamemodeParameters.js

--- client/room/gamemodeParameters.js
+++
@@ -1,8 +0,0 @@
-
-
-export function SetInventoryBlue() {
- switch (GameMode.Parameters.GetString('Blue')) {
-     case 'PNusto': blueTeam.Inventory.Melee.Value = false; break;
-     case 'Melee': blueTeam.Inventory.Melee.Value = true; break;
-   }
-}
