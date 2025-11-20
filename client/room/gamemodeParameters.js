b0099a5a431cd1e2928583809b78a11b440c7c87
ƬN丅 ｌlᴠɆ (ᴼᶠᶠⁱᶜⁱᵃˡ) <145314654+hoycmd@users.noreply.github.com>
2025-11-20 13:58:27 +0700

    Update gamemodeParameters.js

--- client/room/gamemodeParameters.js
+++ client/room/gamemodeParameters.js
@@ -14,10 +14,9 @@ export function GameModeMatchTime() {
     return 421;
 }
 
-/* export function SetInventoryBlue() {
+export function SetInventoryBlue() {
  switch (GameMode.Parameters.GetString('Blue')) {
      case 'PNusto': blueTeam.Inventory.Melee.Value = false; break;
      case 'Melee': blueTeam.Inventory.Melee.Value = true; break;
    }
 }
-*/
