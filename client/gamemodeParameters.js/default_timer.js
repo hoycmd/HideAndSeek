2127d07dcd8d112eaea8f028897a23524aa2274b
ƬN丅 ｌlᴠɆ (ᴼᶠᶠⁱᶜⁱᵃˡ) <145314654+hoycmd@users.noreply.github.com>
2025-11-20 20:02:20 +0700

    Update default_timer.js

--- client/gamemodeParameters.js/default_timer.js
+++ client/gamemodeParameters.js/default_timer.js
@@ -1,1 +1,14 @@
+import { GameMode } from 'pixel_combats/room';
 
+// * Константы имён, объектов. * //
+const length = GameMode.Parameters.GetString('GameLength');
+
+// * Длинна, матча. * //
+export function GameModeMatchTime() {
+    switch (length) {
+        case 'Min10MatchTime': return 601;
+        case 'Min7MatchTime': return 421;
+        case 'Min5MatchTime': return 301;
+    }   
+    return 421;
+}
