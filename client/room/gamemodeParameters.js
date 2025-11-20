1aac75a95216263c77bdf8e381974c451bd579e6
ƬN丅 ｌlᴠɆ (ᴼᶠᶠⁱᶜⁱᵃˡ) <145314654+hoycmd@users.noreply.github.com>
2025-11-20 20:01:51 +0700

    Update gamemodeParameters.js

--- client/room/gamemodeParameters.js
+++ client/room/gamemodeParameters.js
@@ -1,18 +1,4 @@
 
-import { GameMode, Inventory } from 'pixel_combats/room';
-
-// * Константы имён, объектов. * //
-const length = GameMode.Parameters.GetString('GameLength');
-
-// * Длинна, матча. * //
-export function GameModeMatchTime() {
-    switch (length) {
-        case 'Min10MatchTime': return 601;
-        case 'Min7MatchTime': return 421;
-        case 'Min5MatchTime': return 301;
-    }   
-    return 421;
-}
 
 export function SetInventoryBlue() {
  switch (GameMode.Parameters.GetString('Blue')) {
