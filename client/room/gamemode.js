d28579bf036d96f5c5923098d880faff295bfd3d
ƬN丅 ｌlᴠɆ (ᴼᶠᶠⁱᶜⁱᵃˡ) <145314654+hoycmd@users.noreply.github.com>
2025-11-23 00:57:31 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -94,7 +94,7 @@ redTeam.Properties.Get('Deaths').Value = redTeam.Count;
 // * Обработчик спавнов: авто-бессмертие после респавна игрока. (Т3) * //
 Spawns.OnSpawn.Add(p => {
  // * Если игрок вне команд, то спавны не начисляем. * //
- if (p.Team === null) continue;
+// if (p.Team === null) continue;
 // * Засчёт спавнов игрока после респавна. * //
 ++p.Properties.Spawns.Value;
 // * Бессмертие игрока после респавна. * //
@@ -103,7 +103,8 @@ Spawns.OnSpawn.Add(p => {
 });
 // * Если стёк таймер бессмертия, то отключаем защиту. * //
 Timers.OnPlayerTimer.Add(t => {
- if (t.id != 'Immortality') return t.Player.Properties.Immortality.Value = false;
+ if (t.id != 'Immortality') return; 
+ t.Player.Properties.Immortality.Value = false;
 });
 	
 // * Обработчик смертей. * //
